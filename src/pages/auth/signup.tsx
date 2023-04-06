d7695cce4ad69e7daa864aa7003d4358157e9aa3
StivenF0 <stivenfonseca@gmail.com>
2023-04-05 22:04:35 -0300

    Created the pages of signin and signup with react-icons installed

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { HiOutlineMail } from 'react-icons/hi';
+import { AiOutlineUser } from 'react-icons/ai';
 import { BiKey } from 'react-icons/bi';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import { useState } from "react";
@@ -43,7 +44,7 @@ export default function SignInPage() {
 
   return <>
     <Head>
-      <title>Sign In</title>
+      <title>Sign Up</title>
     </Head>
     <main
       className="w-full min-h-screen grid place-items-center bg-dark"
@@ -51,8 +52,20 @@ export default function SignInPage() {
       <form 
         className="flex flex-col min-w-[20rem] w-[20vw] max-w-[30rem] aspect-[2/3] bg-gray-900 rounded-xl p-6"
       >
-        <h1 className="text-gray-100 font-bold text-4xl">Sign In</h1>
+        <h1 className="text-gray-100 font-bold text-4xl">Sign Up</h1>
         <div className="flex flex-col flex-grow items-center justify-center">
+          <div className="flex items-center border-b border-gray-100 w-[85%]">
+            <AiOutlineUser 
+              className="fill-slate-300 text-xl"
+            />
+            <div className="px-[0.2rem]" />
+            <input
+              type="text"
+              placeholder="Name"
+              className="flex-grow bg-transparent focus:outline-none text-gray-100 placeholder:text-gray-600"
+            />
+          </div>
+          <div className="p-3"/>
           <div className="flex items-center border-b border-gray-100 w-[85%]">
             <HiOutlineMail 
               className="stroke-slate-300 text-xl"
@@ -72,13 +85,13 @@ export default function SignInPage() {
           <div className="w-[85%]">
             <input
               type="submit" 
-              value="Sign In" 
+              value="Sign Up" 
               className="border-2 border-slate-300 w-full h-full py-1 text-slate-300 rounded-xl font-bold text-lg cursor-pointer" 
             />
           </div>
         </div>
         <div className="p-2" />
-        <div className="text-gray-100">Not registered yet? <Link className="text-aigreen font-semibold" href="/auth/signup">Sign Up</Link></div>
+        <div className="text-gray-100">Alredy registered? <Link className="text-aigreen font-semibold" href="/auth/signin">Sign In</Link></div>
       </form>
     </main>
   </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     extend: {
       colors: {
         "dark": "#343541",
+        "aigreen": "#75a99c",
       }
     },
   },
